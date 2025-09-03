06accc9b0adb5a9f7d2362b104d07b4eb35deebf
Matveychik1 <matvijpro25@gmail.com>
2025-09-03 13:52:25 +0300

    JVSCR
    12123

---
+++ JS/WEB/myjs/one.js
@@ -0,0 +1,11 @@
+        var name ="Петро Щур";
+        name = prompt("Хто ти воїй?");
+        console.log("name = ", name);
+        alert("Красавчік"+ name );
+str="chizas";
+        a=52
+
+        document.writeln(a);
+        document.writeln("a ="+a);
+        document.writeln("<b>"+str+"</b>");
+        console.log("іба чотко")
